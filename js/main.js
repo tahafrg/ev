636c6f69fa8d133c3c0510fd9ec7634fd4c448b1
Taha <tahafrg@hotmail.com>
2018-07-05 14:06:18 +0200

    Creating the UI structure and main layout

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+    $(".heading-compose").click(function() {
+        $(".side-two").css({
+          "left": "0"
+        });
+      });
+  
+      $(".newMessage-back").click(function() {
+        $(".side-two").css({
+          "left": "-100%"
+        });
+      });
